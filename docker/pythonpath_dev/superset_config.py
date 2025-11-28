9b5605b7724f22109bf8b92ed62282f4bbda0b46
raviteja-terra <raviteja.bora@terralogic.com>
2025-11-28 10:25:31 +0530

    Update superset_config.py

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -42,6 +42,8 @@ EXAMPLES_HOST = os.getenv("EXAMPLES_HOST")
 EXAMPLES_PORT = os.getenv("EXAMPLES_PORT")
 EXAMPLES_DB = os.getenv("EXAMPLES_DB")
 
+SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY")
+
 # The SQLAlchemy connection string.
 SQLALCHEMY_DATABASE_URI = (
     f"{DATABASE_DIALECT}://"
